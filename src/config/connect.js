2d31fc5d15ab99a54c0ed09d6b1344be0dd84e60
Elvis Vedia Cruz <elvis199vedia9@gmail.com>
2026-05-05 09:32:46 -0400

    cambios en la bd

--- src/config/connect.js
+++ src/config/connect.js
@@ -4,7 +4,16 @@ const sequelize = new Sequelize(
     process.env.DB_URL,
     {
         dialect: "mysql",
-        logging: false
+        logging: false,
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 20000,
+            idle: 10000,
+        },
+        dialectOptions: {
+            connectTimeout: 10000,
+        },
     }
 );
 
